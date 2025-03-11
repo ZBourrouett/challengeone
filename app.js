becc89c0e817b643e7b98da27f0f4f0768401e3b
ZBourrouett <zamorasteven0204@gmail.com>
2025-03-10 18:26:55 -0600

    añadiendo

---
+++ app.js
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+=======
+
+>>>>>>> ae94c7b7e9506b6d14592b324543a834f11593b6
